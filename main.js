098fca340ab9316bb9795eaab9e0016900cfd890
Shaikh Muzammil <shaikhmuzammil9937@gmail.com>
2024-10-25 13:01:12 +0530

    product modified, images changed, responsive going

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('.main-div').addEventListener('mouseover', function() {
+    document.querySelector('.best-deal-img').src = './electronic-store-product-gallery-image-6-600x600-1.jpeg';
+});
+
+document.querySelector('.main-div').addEventListener('mouseout', function() {
+    document.querySelector('.best-deal-img').src = './electronic-store-product-image-27-300x300-1.jpeg';
+});
